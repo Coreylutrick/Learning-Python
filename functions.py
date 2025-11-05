7a9e41f4d6200e9fa0bcfc5c298c0d14e38a562c
Corey Lutrick <coreylutrick@gmail.com>
2025-11-04 21:59:04 -0600

    attempting to apply previous lessons toogether to make a function that will add 2 user input numbers together. I have the function working as long as the user put in an integer. trying to find a solution for if they put in a non integer character.

--- functions.py
+++ functions.py
@@ -1,15 +1,31 @@
+import sys
 # def hello_world():
 #     print("Hellow World")
 
 # hello_world()
+user_choice = input("Please enter a number\n")
+user_choice2 = input("Please enter a second number\n")
+new_user_choice = int(user_choice)
+new_user_choice2 = int(user_choice2)
 
-# def sum(num1=0, num2=0):
-#     if(type(num1) is not int or type(num2) is not int):
-#         return 0
-#     return num1 + num2
+def check_if_int(num1, num2):
+    if(type(num1) is not int or type(num2) is not int):
+        print("Please pick a number")
+        return
+    else:
+        print("The total of the numbers is ")
+        return num1 + num2
 
-# total = sum(7, 2)
-# print(total)
+
+def sum(num1, num2):
+    if(type(num1) is not int or type(num2) is not int):
+        return 0
+    return num1 + num2
+
+
+total = check_if_int(new_user_choice, new_user_choice2)
+# print("The total of the numbers is")
+print(total)
 
 # def multiple_items(*args):
 #     print(args)
@@ -17,8 +33,8 @@
 
 # multiple_items("Corey", "Logan", "Silas")
 
-def multi_named_items(**kwargs):
-    print(kwargs)
-    print(type(kwargs))
+# def multi_named_items(**kwargs):
+#     print(kwargs)
+#     print(type(kwargs))
 
-multi_named_items(first = "Corey", last = "Silas")
+# multi_named_items(first = "Corey", last = "Silas")
