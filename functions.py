0f9d0dddee7d84611cde7af57577be94af0c60c9
Corey Lutrick <coreylutrick@gmail.com>
2025-11-05 13:31:54 -0600

    still hitting my head on the wall trying to figure out within a function how to change a user input string to an integer and if it cant be changed to then exit the function

--- functions.py
+++ functions.py
@@ -5,26 +5,28 @@ import sys
 # hello_world()
 user_choice = input("Please enter a number\n")
 user_choice2 = input("Please enter a second number\n")
-new_user_choice = int(user_choice)
-new_user_choice2 = int(user_choice2)
+# new_user_choice = int(user_choice)
+# new_user_choice2 = int(user_choice2)
 
 def check_if_int(num1, num2):
+    int(num1)
+    int(num2)
+    print(type(num1))
     if(type(num1) is not int or type(num2) is not int):
-        print("Please pick a number")
-        return
+        print("sorry thats not a number")
+        return 
     else:
-        print("The total of the numbers is ")
         return num1 + num2
-
-
+    
 def sum(num1, num2):
     if(type(num1) is not int or type(num2) is not int):
         return 0
     return num1 + num2
 
 
-total = check_if_int(new_user_choice, new_user_choice2)
-# print("The total of the numbers is")
+total = check_if_int(user_choice, user_choice2)
+
+print("The total of the numbers is")
 print(total)
 
 # def multiple_items(*args):
